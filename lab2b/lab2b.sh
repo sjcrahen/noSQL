d9840324c84746e6e3abbb0f200b2f91efbeef11
Shawn Crahen <shawn.j.crahen@gmail.com>
2021-06-20 18:39:33 -0400

    lab2-3

--- lab2b/lab2b.sh
+++ lab2b/lab2b.sh
@@ -10,11 +10,17 @@ curl -vSsf -X POST http://riak:8098/mapred -H 'Content-Type: application/json' -
 
 printf "\n\n<--- Curl command below to: 1. A map-only query for finding all days where the low is less than $450.00. --->\n"
 
-
-
-
+curl -X POST http://riak:8098/mapred -H 'Content-Type: application/json' -d \
+	'{"inputs":"goog","query":[{"map":{"language":"javascript","source",
+	"function(value,keyData,arg) {var data = Riak.mapValuesJson(value)[0];
+	if (data.Low && data.Low < 450.00) return [value.key]; else return [];
+	}","keep":true}}]}'
 
 
 printf "\n\n<--- Curl command below to: 2. A map-only query for finding all days where the close was lower than the open --->\n"
 
-
+curl -X POST http://riak:8098/mapred -H 'Content-Type: application/json' -d \
+	'{"inputs":"goog","query":[{"map":{"language":"javascript","source",
+	"function(value,keyData,arg) {var data = Riak.mapValuesJson(value)[0];
+	if (data.Close < data.Open) return [value.key]; else return [];
+	}","keep":true}}]}'
